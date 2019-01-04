c917480d697ba159795661f9907193d2b5f31041
mengzhaolis <1912882425@qq.com>
2019-01-04 17:45:23 +0800

    农商

---
+++ pages/index/index.js
@@ -0,0 +1,44 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      '/images/index/1.jpg',
+      '/images/index/2.jpg',
+      '/images/index/3.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    first: '/images/index/xianggua.jpg',
+    sec: '/images/index/yangjiaomi.jpg',
+    ad: '/images/index/putao.jpg',
+    dataImg: [
+      '/images/index/xianggua1.jpg',
+      '/images/index/xianggua2.jpg',
+      '/images/index/putao1.jpg',
+      '/images/index/yangjiaomi1.jpg',
+      '/images/index/yangjiaomi2.jpg',
+    ]
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
